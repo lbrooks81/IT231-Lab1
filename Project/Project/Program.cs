6c94364dbc424c418566324d9a4ca3fedec79ade
lbrooks81 <143453730+lbrooks81@users.noreply.github.com>
2024-12-05 15:13:10 -0500

    math add

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -2,9 +2,31 @@
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            int num1 = 0;
+            int num2 = 0;
 
+            Console.Write("Enter an integer: ");
+
+            while (!int.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.Write("Incorrect type, please enter an integer: ");
+            }
+
+            Console.Write("Enter another integer: ");
+
+            while (!int.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.Write("Incorrect type, please enter an integer: ");
+            }
+
+            DoMath(num1, num2);
+        }
+
+        public static void DoMath(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
         }
     }
 }
